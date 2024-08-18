c9e943f983a601882141baf46d7c8a9552f8c728
Luka <lukabrkovic@proton.me>
2024-08-18 14:40:16 +0200

    chorr: init project

---
+++ src/test/kotlin/com/vemeet/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vemeet.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
